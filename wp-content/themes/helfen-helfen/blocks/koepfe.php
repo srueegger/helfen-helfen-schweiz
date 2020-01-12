5638345e5c76e01063cb326d4eeb34923ea7978b
Samuel Rüegger <samuel.ruegger@compresso.ch>
2020-01-12 15:17:46 +0100

    extend Kopf Page

--- wp-content/themes/helfen-helfen/blocks/koepfe.php
+++ wp-content/themes/helfen-helfen/blocks/koepfe.php
@@ -29,48 +29,67 @@ if( !empty($block['align']) ) {
 			global $post;
 			foreach($koepfe as $post) {
 				setup_postdata( $post );
-				$image = get_field('kopf_image', get_the_ID());
-				?>
-				<div class="col-12 col-sm-6 col-xl-4 kopfItem">
-					<picture>
-						<source data-srcset="<?php echo $image['sizes']['kopf']; ?> 1x, <?php echo $image['sizes']['kopf2x']; ?> 2x">
-						<img class="lazy" data-src="<?php echo $image['sizes']['kopf']; ?>" alt="<?php echo $image['alt']; ?>" />
-					</picture>
-					<div class="overlay"></div>
-					<div class="titleTxt">
-						<?php the_title('<h2>', '</h2>'); ?>
-						<p class="mb-0"><?php the_field('kopf_job', get_the_ID()); ?></p>
-					</div>
-					<div class="quoteInfos">
-						<div class="quote w-75 mx-auto mb-5">
-							<blockquote class="blockquote">
-								<p class="mb-0 text-center text-white">"<?php the_field('kopf_quote', get_the_ID()); ?>"</p>
-							</blockquote>
+				$typ = get_field('kopf_typ', get_the_ID());
+				if(!$typ) {
+					/* Normaler Kopf ausgeben */
+					$image = get_field('kopf_image', get_the_ID());
+					?>
+					<div class="col-12 col-sm-6 col-xl-4 kopfItem">
+						<picture>
+							<source data-srcset="<?php echo $image['sizes']['kopf']; ?> 1x, <?php echo $image['sizes']['kopf2x']; ?> 2x">
+							<img class="lazy" data-src="<?php echo $image['sizes']['kopf']; ?>" alt="<?php echo $image['alt']; ?>" />
+						</picture>
+						<div class="overlay"></div>
+						<div class="titleTxt">
+							<?php the_title('<h2>', '</h2>'); ?>
+							<p class="mb-0"><?php the_field('kopf_job', get_the_ID()); ?></p>
 						</div>
-						<div class="infoContent ml-4">
-							<a href="mailto:<?php the_field('kopf_mail', get_the_ID()); ?>">
-								<span class="fa-stack fa-2x">
-									<i class="fas fa-circle fa-stack-2x"></i>
-									<i class="fas fa-envelope fa-stack-1x fa-inverse"></i>
-								</span>
-							</a>
-							<?php if(get_field('kopf_sm', get_the_ID()) != -1) { ?>
-							<a href="<?php the_field('kopf_sm_link', get_the_ID()); ?>" target="_blank">
-								<span class="fa-stack fa-2x">
-									<i class="fas fa-circle fa-stack-2x"></i>
-									<i class="fab fa-<?php the_field('kopf_sm', get_the_ID()); ?> fa-stack-1x fa-inverse"></i>
-								</span>
-							</a>
-							<?php } ?>
+						<div class="quoteInfos">
+							<div class="quote w-75 mx-auto mb-5">
+								<blockquote class="blockquote">
+									<p class="mb-0 text-center text-white">"<?php the_field('kopf_quote', get_the_ID()); ?>"</p>
+								</blockquote>
+							</div>
+							<div class="infoContent ml-4">
+								<a href="mailto:<?php the_field('kopf_mail', get_the_ID()); ?>">
+									<span class="fa-stack fa-2x">
+										<i class="fas fa-circle fa-stack-2x"></i>
+										<i class="fas fa-envelope fa-stack-1x fa-inverse"></i>
+									</span>
+								</a>
+								<?php if(get_field('kopf_sm', get_the_ID()) != -1) { ?>
+								<a href="<?php the_field('kopf_sm_link', get_the_ID()); ?>" target="_blank">
+									<span class="fa-stack fa-2x">
+										<i class="fas fa-circle fa-stack-2x"></i>
+										<i class="fab fa-<?php the_field('kopf_sm', get_the_ID()); ?> fa-stack-1x fa-inverse"></i>
+									</span>
+								</a>
+								<?php } ?>
+							</div>
 						</div>
 					</div>
-				</div>
-				<?php
+					<?php
+				} else {
+					/* Textbox ausgeben */
+					?>
+					<div class="col-12 col-sm-6 col-xl-4 kopfItem txtItem <?php the_field('kopf_txt_color', get_the_ID()); ?>">
+						<h3><?php the_field('kopf_txt_title1', get_the_ID()); ?></h3>
+						<h4><?php the_field('kopf_txt_title2', get_the_ID()); ?></h4>
+						<?php
+						$link = get_field('', get_the_ID());
+						$link_target = $link['target'] ? $link['target'] : '_self';
+						?>
+						<a href="<?php echo $link['url']; ?>" target="<?php echo $link_target; ?>" class="hh-btn">
+							<div class="fullpageArrowBottom">
+								<i class="fas fa-angle-right fa-3x"></i>
+							</div>
+						</a>
+					</div>
+					<?php
+				}
 			}
 			wp_reset_postdata();
 		}
 		?>
-		<div class="col-12 col-sm-6 col-xl-4 kopfItem bg-primary"></div>
-		<div class="col-12 col-sm-6 col-xl-4 kopfItem bg-secondary"></div>
 	</div>
 </div>
