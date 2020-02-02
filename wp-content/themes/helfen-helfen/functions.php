e74341d7579f586e164c840e98d8278991c8138b
Samuel Rüegger <samuel@rueegger.me>
2020-02-02 13:54:52 +0100

    some CSS Changes

--- wp-content/themes/helfen-helfen/functions.php
+++ wp-content/themes/helfen-helfen/functions.php
@@ -160,7 +160,7 @@ add_action( 'after_setup_theme', 'hh_gutenberg_colors', 11 );
 add_filter( 'render_block', function( $block_content, $block ) {
 	if(!is_front_page() && !is_page('presse')) {
 		// Target core/* and core-embed/* blocks.
-		if ( preg_match( '~^core/|core-embed/~', $block['blockName'] ) && $block['blockName'] != 'core/cover' && $block['blockName'] != 'core/gallery' ) {
+		if ( preg_match( '~^core/|core-embed/~', $block['blockName'] ) && $block['blockName'] != 'core/cover' && $block['blockName'] != 'core/gallery' && $block['blockName'] != 'core/column' && $block['blockName'] != 'core/columns'  ) {
 			$block_content = sprintf( '<div class="container"><div class="row justify-content-center"><div class="col-12 col-lg-9">%s</div></div></div>', $block_content );
 		} elseif ($block['blockName'] == 'core/gallery') {
 			$block_content = sprintf( '<div class="container-fluid"><div class="row justify-content-center"><div class="col-12 col-lg-9">%s</div></div></div>', $block_content );
